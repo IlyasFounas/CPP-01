0746691f6c3e2b35cc9abd763b6e6803b61a97d5
Ilyas Founas <ifounas@student.42lyon.fr>
2025-10-15 08:39:15 +0200

    last commit

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -4,6 +4,10 @@ int main(int argc, char **argv)
 {
     Harl turbofaker;
     if (argc != 2)
+    {
+        std::cout << "How to launch the executable : " << std::endl;
+        std::cout << "./harlFilter <string>" << std::endl;
         return (0);
+    }
     turbofaker.complain(argv[1]);
 }
